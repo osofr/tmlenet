b859f76344b5cf9d51b83f34250a86730af183e4
Oleg Sofrygin <olegso@gmail.com>
2015-10-18 15:13:13 -0700

    adding the draft vignette;

--- tests/RUnit/RUnit_tests_00a.R
+++ tests/RUnit/RUnit_tests_00a.R
@@ -25,7 +25,8 @@ if(FALSE) {
   # devtools::check() # runs check with devtools
   # devtools::check(args = c("--no-vignettes"), build_args = c("--no-build-vignettes")) # runs check with devtools
   # devtools::build_win(args = "--compact-vignettes") # build package on CRAN servers (windows os?)
-  devtools::build()
+  # devtools::build()
+  devtools::build(args = "--compact-vignettes")
   devtools::build_win(args = "--as-cran") # build package on CRAN servers (windows os?)
   # devtools::build_win(args = "--compact-vignettes") # build package on CRAN servers (windows os?)
   # devtools::build(args = "--compact-vignettes") # build package tarball compacting vignettes
